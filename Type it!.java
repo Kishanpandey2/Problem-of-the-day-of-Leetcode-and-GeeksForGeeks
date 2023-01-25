e3b40a960a753c64a8261c48ef9638aa54cd7d94
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2023-01-25 16:14:15 +0530

    GFG PODT

---
+++ Type it!.java
@@ -0,0 +1,17 @@
+class Solution {
+    int minOperation(String s) {
+        
+        int len = s.length() ;
+        int low = 0 , high = len ;
+        if(len==1)
+        return 1 ;
+        while(low<=high)
+        {
+            int mid = low+high/2 -1 ;
+            if((s.substring(0,mid+1)).equals(s.substring(mid+1,2*(mid+1))))
+            return (mid+1+1+(len-2*(mid+1))) ;
+            high--;
+        }
+        return len ;
+    }
+}
