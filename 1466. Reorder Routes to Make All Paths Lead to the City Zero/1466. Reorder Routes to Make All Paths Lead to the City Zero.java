f284915c35206dd41d59570f95d25665a5868967
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2023-03-24 22:36:20 +0530

    Time: 54 ms (69.86%), Space: 65 MB (86.66%) - LeetHub

---
+++ 1466. Reorder Routes to Make All Paths Lead to the City Zero/1466. Reorder Routes to Make All Paths Lead to the City Zero.java
@@ -0,0 +1,20 @@
+class Solution {
+   int dfs(List<List<Integer>> al, boolean[] visited, int from) {
+        int change = 0;
+        visited[from] = true;
+        for (var to : al.get(from))
+            if (!visited[Math.abs(to)])
+                change += dfs(al, visited, Math.abs(to)) + (to > 0 ? 1 : 0);
+        return change;   
+    }
+    public int minReorder(int n, int[][] connections) {
+        List<List<Integer>> al = new ArrayList<>();
+        for(int i = 0; i < n; ++i) 
+            al.add(new ArrayList<>());
+        for (var c : connections) {
+            al.get(c[0]).add(c[1]);
+            al.get(c[1]).add(-c[0]);
+        }
+        return dfs(al, new boolean[n], 0);
+    }
+}
