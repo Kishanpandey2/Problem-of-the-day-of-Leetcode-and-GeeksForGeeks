6730687e73284e1e2d0a772a835e20c8a060bdd9
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2023-05-04 23:11:40 +0530

    Time: 12 ms (64.20%), Space:  43.4 MB (22.16%) - LeetHub

---
+++ 649. Dota2 Senate/649. Dota2 Senatef.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> qr = new LinkedList<>();
+        Queue<Integer> qd = new LinkedList<>();
+        int n = senate.length();
+        for(int i = 0;i < n;i++){
+            if(senate.charAt(i) == 'R') qr.add(i);
+            else qd.add(i);
+        }
+        for(;!qr.isEmpty() && !qd.isEmpty();){
+            int r_i = qr.poll();
+            int d_i = qd.poll();
+            if(r_i < d_i) qr.add(r_i + n);
+            else qd.add(d_i + n);
+        }
+        return qr.size() > qd.size() ? "Radiant" : "Dire";
+    }
+}
