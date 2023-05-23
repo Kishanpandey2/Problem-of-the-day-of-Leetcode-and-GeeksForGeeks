ba6e096fd5b6a0e77140d88b38c2300f5847c882
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2023-05-23 23:58:27 +0530

    Time: 17 ms (53.19%), Space: 46.8 MB(47.48%) - LeetHub

---
+++ 703. Kth Largest Element in a Stream/703. Kth Largest Element in a Stream.java
@@ -0,0 +1,22 @@
+class KthLargest {
+    private int k;
+    private PriorityQueue<Integer> minHeap;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        minHeap = new PriorityQueue<>(k);
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.offer(val);
+        } else if (val > minHeap.peek()) {
+            minHeap.poll();
+            minHeap.offer(val);
+        }
+        return minHeap.peek();
+    }
+}
