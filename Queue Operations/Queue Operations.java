874bf9bf7a6aa0020cd14ed001ef51c96c10a31d
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2023-06-17 23:58:36 +0530

    GFG PODT

---
+++ Queue Operations/Queue Operations.java
@@ -0,0 +1,24 @@
+class Geeks{
+    
+     
+    static void insert(Queue<Integer> q, int k){
+        
+        
+        q.add(k);
+        
+    }
+    
+
+    static int findFrequency(Qu  // Your code here
+        int count=0;
+        for(Integer e:q)
+        {
+            if(e==k)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+}
