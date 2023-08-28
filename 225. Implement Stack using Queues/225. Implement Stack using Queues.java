59cfffb9fb41980175b274defb364cee90409039
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2023-08-28 22:56:52 +0530

    Time: 0 ms (100%), Space: 38.89 MB (98.21%) - LeetHub

---
+++ 225. Implement Stack using Queues/225. Implement Stack using Queues.java
@@ -0,0 +1,26 @@
+public class MyStack {
+    private Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q.add(x);
+        for (int i = 1; i < q.size(); i++) {
+            q.add(q.remove());
+        }
+    }
+
+    public int pop() {
+        return q.remove();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
