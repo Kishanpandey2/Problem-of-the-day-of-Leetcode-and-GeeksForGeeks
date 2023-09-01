c9533253ca4d7c4e268a943286aa91a2f9d8df35
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2023-09-01 22:56:20 +0530

    Time: 2 ms (73.73%), Space: 46.36 MB (66.16%) - LeetHub

---
+++ 338. Counting Bits/338. Counting Bits.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] countBits(int n) {
+        int []ans = new int[n+1] ;
+        ans[0]= 0 ;
+        for(int i = 1;i<=n;i++){
+            ans[i] = ans[i>>1]+(i%2) ;
+        }
+        return ans ;
+    }
+}
