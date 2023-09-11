727999f58241c0de055dc583f0e414635287d2fb
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2023-09-11 20:44:29 +0530

    GFG PODT

---
+++ Lucky Numbers/Lucky Numbers.java
@@ -0,0 +1,15 @@
+
+
+class Solution
+{
+    public static boolean isLucky(int n)
+    {
+       for(int i = 2;i<=n;i++){
+           if(n%i == 0)
+           return false ;
+           
+           n = n - n/i ;
+       }
+       return true;
+    }
+}
