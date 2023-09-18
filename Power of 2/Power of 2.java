67963e4266f89e9818afcf424762c3499ce767b4
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2023-09-18 22:17:03 +0530

    GFG PODT

---
+++ Power of 2/Power of 2.java
@@ -0,0 +1,14 @@
+
+
+class Solution{
+    
+   
+    public static boolean isPowerofTwo(long n){
+        
+        if(n == 0)return false;
+       
+       return (n & n-1)==0;
+        
+    }
+    
+}
