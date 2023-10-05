86de3b1866ec9d6a545908fe50bdd294daa3db5b
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2023-10-06 00:29:41 +0530

    GFG PODT

--- Count number of substrings/Count number of substrings.java
+++ Count number of substrings/Count number of substrings.java
@@ -1,3 +1,4 @@
+// Brute Force solution
 
 class Solution
 {
@@ -25,5 +26,58 @@ class Solution
        }
        
        return ans;
+    }
+}
+
+
+// Method 2
+class Solution
+{
+    long substrCount (String S, int K) {
+        return helper(S,K)-helper(S,K-1);
+    }
+    
+    
+    public static long helper(String S,int k){
+        
+        int[] arr=new int[26];
+        long ans=0;
+        int diff=0;
+        int p=0;
+        
+        for(int i=0;i<S.length();i++){
+            char ch=S.charAt(i);
+            arr[ch-'a']++;
+            
+            
+            if(arr[ch-'a']==1){
+                diff++;                          
+                
+                  }
+            
+            if(diff<=k){
+            }
+            else{
+                
+                while(diff>k){
+                    char ch1= S.charAt(p);
+                    
+                    arr[ch1-'a']--;
+                    p++;
+                    if(arr[ch1-'a']==0){
+                        
+                        
+                        diff--;
+                    }
+                    
+                    if(diff<=k){
+                        
+                    }
+                }
+            }
+            
+            ans+=i-p+1;
+        }
+        return ans;
     }
 }
