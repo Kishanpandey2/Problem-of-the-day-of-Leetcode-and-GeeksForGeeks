005e0a4c8522d4e0271d22eca1b22b3f18917141
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2023-10-26 00:09:26 +0530

    Time: 0 ms (100%), Space: 38.89 MB (80.10%) - LeetHub

---
+++ 779. K-th Symbol in Grammar/779. K-th Symbol in Grammar.java
@@ -0,0 +1,14 @@
+class Solution {  
+    public int kthGrammar(int n, int k) {
+       
+        if(n == 1 &&  k == 1)return 0 ;
+
+        int mid = (int)Math.pow(2,n-1)/2 ;
+
+        if(k<=mid)
+        return  kthGrammar(n-1,k);
+        else
+        
+        return   1-kthGrammar(n-1,k - mid);
+    }
+}
