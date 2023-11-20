1d64330f667a5128f981bd0f0bbb27d7f8b69888
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2023-11-20 23:01:23 +0530

    Time: 1 ms (100%), Space: 62.07 MB (39.20%) - LeetHub

---
+++ 2391. Minimum Amount of Time to Collect Garbage/2391. Minimum Amount of Time to Collect Garbage.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int garbageCollection(String[] garbage, int[] travel) {
+        int n = garbage.length;
+        int ans = 0;
+        for (int i = 0; i < n - 1; i++) {
+            ans += 3 * travel[i];
+        }
+        for (String s : garbage) {
+            ans += s.length();
+        }
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("G")) {
+                ans -= travel[i - 1];
+            } else {
+                break;
+            }
+        }
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("P")) {
+                ans -= travel[i - 1];
+            } else {
+                break;
+            }
+        }
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("M")) {
+                ans -= travel[i - 1];
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+}
