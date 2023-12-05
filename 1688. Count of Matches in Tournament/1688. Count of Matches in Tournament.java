cace0e09c23a973bf9d19ea15d3e64cd24720430
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2023-12-05 20:51:55 +0530

    Time: 0 ms (100%), Space: 40.11 MB (5.39%) - LeetHub

---
+++ 1688. Count of Matches in Tournament/1688. Count of Matches in Tournament.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numberOfMatches(int n) {
+         int ans = 0 ;
+
+         while(n>1){
+             ans+=(n/2) ;
+             n = n - n/2 ;
+         }
+
+         return ans;
+    }
+}
