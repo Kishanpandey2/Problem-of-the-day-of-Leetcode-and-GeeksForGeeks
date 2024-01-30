605f039a5b260f8fb69bea59c90ff34afc10b546
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2024-01-30 22:00:42 +0530

    GFG PODT

---
+++ LCS of three strings/LCS of three strings.java
@@ -0,0 +1,24 @@
+
+class Solution 
+{ 
+  int [][][]dp; 
+    
+    int LCSof3(String A, String B, String C, int n1, int n2, int n3) 
+    { 
+      dp= new int[n1+1][n2+1][n3+1];
+      for(int i = 1;i<=n1;i++)
+      {
+          for(int j = 1 ;j<=n2;j++){
+          for(int k = 1 ;k<=n3;k++){
+              if( (A.charAt(i-1)== B.charAt(j-1)) &&  (B.charAt(j-1)== C.charAt(k-1)) )
+              dp[i][j][k] = 1 +   dp[i-1][j-1][k-1] ;
+              else
+              dp[i][j][k] = Math.max(dp[i-1][j][k],Math.max(dp[i][j-1][k],dp[i][j][k-1]));
+          }
+          }
+      }
+       
+       return dp[n1][n2][n3];
+    }
+   
+}
