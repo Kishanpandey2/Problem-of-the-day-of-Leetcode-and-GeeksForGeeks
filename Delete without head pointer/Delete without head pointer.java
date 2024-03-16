714fabb8019e06b1187563633f6ed338a6583a36
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2024-03-16 15:47:45 +0530

    GFG PODT

---
+++ Delete without head pointer/Delete without head pointer.java
@@ -0,0 +1,25 @@
+
+/*
+class Node
+{
+	int data ;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+*/
+
+//Function to delete a node without any reference to head pointer.
+class Solution
+{
+    void deleteNode(Node del_node)
+    {
+         del_node.data = del_node.next.data ;
+         del_node.next = del_node.next.next ;
+         
+    }
+}
+
