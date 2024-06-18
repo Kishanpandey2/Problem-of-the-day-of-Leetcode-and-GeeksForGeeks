dd6ff90bf2df4b856a5b4a8563c8ef30fa70bf9b
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2024-06-18 22:42:02 +0530

    GFG PODT

---
+++ Number of Rectangles in a Circle/Number of Rectangles in a Circle.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    int rectanglesInCircle(int r) {
+        int ans= 0 ;
+        for(int x = 1 ;x<=2*r;x++)
+        {
+            for(int y = 1;y<= 2*r;y++)
+            {
+                if((x*x + y * y) <= (4*r*r))
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
