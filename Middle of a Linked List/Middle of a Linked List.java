ed4a506582021b48c7f6e7f331a974cc25ed6837
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2024-09-12 11:18:04 +0530

    GFG PODT

---
+++ Middle of a Linked List/Middle of a Linked List.java
@@ -0,0 +1,23 @@
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution {
+    int getMiddle(Node head) {
+       Node slow = head;
+       Node fast = head;
+       
+       while(fast != null && fast.next != null)
+       {
+           slow = slow.next ;
+           fast = fast.next.next ;
+       }
+       
+       return slow.data;
+    }
+}
